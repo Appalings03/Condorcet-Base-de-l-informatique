1afd2c506f7c55fcc43946a78f09a45dddc3299b
Appalings03 <martin.biname@gmail.com>
2025-10-08 10:33:32 +0200

    correction de l'exo5

--- Exercices/Structure 1/intervalle2.cpp
+++ Exercices/Structure 1/intervalle2.cpp
@@ -7,6 +7,7 @@ int main()
     double x;
     cout << "Introduiser un réel" << endl;
     cin >> x;
+    // Code avec else if pour chaque intervalle
     /*
     if (x >= 2 && x < 3)
     {
@@ -26,7 +27,16 @@ int main()
     cout << x << " n'appartient pas à I" << endl;
     return 1;
     */
+    // Code avec un seul if et des opérateurs logiques
+    /*
     if ((x >= 2 && x < 3)||(x > 0 && x <= 1)||(x >= -10 && x <=-2))
+    {
+        cout << x << " appartient à I" << endl;
+        return 0;
+    }
+    */
+    // Code avec un seul if et des opérateurs logiques '==', '<' et '!'
+    if ((!(x < 2) && x < 3)||(!(x < 0 && x == 0) && (x < 1 || x == 0))||(!(x == -10 || x < -10) && (x ==-2 || x < -2)))
     {
         cout << x << " appartient à I" << endl;
         return 0;
